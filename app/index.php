ee7b8cc29d455acb9d6b1a081c6aeb6f1bce6358
Danny Huigen <link3321@hotmail.com>
2018-06-27 23:43:49 +0200

    Initial commit

---
+++ app/index.php
@@ -0,0 +1,16 @@
+
+
+
+<?php include_once "templates/html-header.php"; ?>
+
+
+<div class="fs-wrapper">
+
+    <div class="fs-wrapper__inner-wrapper">
+
+    </div>
+
+</div>
+
+
+<?php include_once "templates/html-footer.php"; ?>
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var concat = require("gulp-concat");
+
+//function that fires all functions I need to start developing
+gulp.task("watch" , ["sass"] , function () {
+    console.log("Welcome back enjoy devloping :)");
+    gulp.watch("app/styles/scss/**/*.scss" , ["sass"])
+});
+
+
+
+//function to compile all SCSS from SCSS map to CSS stylesheet
+gulp.task("sass" , function () {
+    return gulp.src("app/styles/scss/**/*.scss")
+        .pipe(sass())
+        .pipe(concat("styles.css"))
+        .pipe(gulp.dest("app/styles/css/"))
+});
