6fcdc6fec3db106500c5e3e2381f0df1ed86b57f
danny <dannyhuigenzakelijk@gmail.com>
2018-06-28 16:52:52 +0200

    adding new features
    push after work

--- app/index.php
+++ app/index.php
@@ -5,11 +5,19 @@
 
 
 <div class="fs-wrapper">
-
     <div class="fs-wrapper__inner-wrapper">
+        <div id="parallax" class="parallax js-parralex">
 
+
+
+            <img data-depth="0.4" class="parallax__floater parallax__floater--danny" src="images/danny-cutout.png" alt="">
+
+            <img data-depth="0.6" class="parallax__floater parallax__floater--background"  alt="">
+
+            <h1 data-depth="0.2" class="parallax__floater parallax__floater--main-title">Ik ben Danny Huigen</h1>
+        </div>
+        
     </div>
-
 </div>
 
 
--- app/templates/html-footer.php
+++ app/templates/html-footer.php
@@ -1,2 +1,10 @@
 </body>
 </html>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="scripts/helpers/tilt.jquery.min.js"></script>
+<script src="scripts/helpers/tilt.jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
+
+
+<script src="scripts/scripts/script.js"></script>
