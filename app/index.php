53d825e3b4fff1b0b25fb46d8818a1fa13be1793
Danny Huigen <link3321@hotmail.com>
2018-06-30 20:04:48 +0200

    Work update
    Adding new background,
    Adding new font + sizes
    Adding Contact button (Not finished)

--- app/index.php
+++ app/index.php
@@ -5,18 +5,32 @@
 
 
 <div class="fs-wrapper">
+
+    <div class="fs-wrapper__bg-lt"></div>
+    <div class="fs-wrapper__bg-rt"></div>
+    <div class="fs-wrapper__bg-rb"></div>
+    <div class="fs-wrapper__bg-lb"></div>
+
+
     <div class="fs-wrapper__inner-wrapper">
         <div id="parallax" class="parallax js-parralex">
 
-
-
             <img data-depth="0.4" class="parallax__floater parallax__floater--danny" src="images/danny-cutout.png" alt="">
+            <a data-depth="0.4" class="parallax__floater parallax__floater--primary-clicker fs-40"  alt="">
+                Contact me
+            </a>
 
-            <img data-depth="0.6" class="parallax__floater parallax__floater--background"  alt="">
+            <div class="parallax__floater parallax__floater--background"  alt=""></div>
 
-            <h1 data-depth="0.2" class="parallax__floater parallax__floater--main-title">Ik ben Danny Huigen</h1>
+
+
+
+
+
+            <h1 data-depth="0.4" class="parallax__floater parallax__floater--main-title fs-70">Hi I am Danny Huigen</h1>
+            <h2 data-depth="0.2" class="parallax__floater parallax__floater--sub-title fs-40">A dutch developer in the making</h2>
+
         </div>
-        
     </div>
 </div>
 
