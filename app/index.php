7bca07459725d552ddc93c441fc046c2539b9135
Danny Huigen <link3321@hotmail.com>
2018-07-01 20:58:05 +0200

    Adding new content
    Finishing btn 1,
    Adding gthub button,
    Adding, mask for Danny img,
    Starting page transition animation

--- app/index.php
+++ app/index.php
@@ -12,25 +12,28 @@
     <div class="fs-wrapper__bg-lb"></div>
 
 
-    <div class="fs-wrapper__inner-wrapper">
+    <div data-page="main" class="js-page_switch js-page_switch--active js-page_switch fs-wrapper__inner-wrapper">
         <div id="parallax" class="parallax js-parralex">
-
-            <img data-depth="0.4" class="parallax__floater parallax__floater--danny" src="images/danny-cutout.png" alt="">
-            <a data-depth="0.4" class="parallax__floater parallax__floater--primary-clicker fs-40"  alt="">
-                Contact me
+            <h1 data-depth="0.6" class="parallax__floater parallax__floater--main-title fs-70">Hi I am Danny Huigen</h1>
+            <h2 data-depth="0.4" class="parallax__floater parallax__floater--sub-title fs-40">A dutch developer in the making</h2>
+            <a data-depth="0.2" class="btn-1 btn-1--mix-blend parallax__floater parallax__floater--contact fs-40"  alt="">
+                <div class="btn-1__text-wrapper">
+                    Contact me
+                </div>
             </a>
-
+            <img data-depth="0.4" class="parallax__floater parallax__floater--danny" src="images/danny-cutout.png" alt="">
+            <a data-depth="0.4" class="btn-1 btn-1--mix-blend parallax__floater parallax__floater--github fs-40"  alt="">
+                <div class="btn-1__text-wrapper">
+                    This project
+                </div>
+            </a>
             <div class="parallax__floater parallax__floater--background"  alt=""></div>
-
-
-
-
-
-
-            <h1 data-depth="0.4" class="parallax__floater parallax__floater--main-title fs-70">Hi I am Danny Huigen</h1>
-            <h2 data-depth="0.2" class="parallax__floater parallax__floater--sub-title fs-40">A dutch developer in the making</h2>
-
+            <div class="parallax__floater parallax__floater--background-hider"  alt=""></div>
         </div>
+    </div>
+
+    <div data-page="contact" class="js-page_switch js-page_switch--hidden js-page_switch fs-wrapper__inner-wrapper">
+
     </div>
 </div>
 
--- app/scripts/scripts/script.js
+++ app/scripts/scripts/script.js
@@ -1,2 +1,63 @@
 var scene = document.getElementById('parallax');
 var parallaxInstance = new Parallax(scene);
+
+
+
+
+$( document ).ready(function() {
+    initPage();
+});
+
+function initPage(){
+
+    $(".js-page_switch[data-page]").each(function () {
+
+        if ($(this).data("page") === "main"){
+            $(this).removeClass("js-page_switch--hidden");
+        }
+
+    });
+
+}
+
+function gotoNewPage(pageName) {
+
+    makePagePassive();
+    setTimeout(function(){
+        showNewPage(pageName);
+        makeNewPageActive(pageName);
+    }, 1500);
+}
+
+function makePagePassive() {
+
+    var targetPageElement = $(".js-page_switch--active");
+
+    targetPageElement.each(function () {
+        $(this).addClass("js-page_switch--passive");
+    });
+
+    setTimeout(function(){
+        targetPageElement.each(function () {
+            $(this).addClass("js-page_switch--hidden");
+        });
+    }, 1500);
+
+    targetPageElement.each(function () {
+        $(this).removeClass("js-page_switch--active");
+    });
+
+}
+
+function showNewPage(pageName) {
+    var targetPageElement = $(".js-page_switch[data-page='"+pageName+"']");
+    targetPageElement.addClass("js-page_switch--passive");
+    targetPageElement.removeClass("js-page_switch--hidden");
+}
+
+function makeNewPageActive(pageName) {
+    var targetPageElement = $(".js-page_switch[data-page='"+pageName+"']");
+    targetPageElement.addClass("js-page_switch--active");
+    targetPageElement.removeClass("js-page_switch--passive");
+}
+
